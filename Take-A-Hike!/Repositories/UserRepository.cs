c96e63f611b0c5c5382bae172ed94cab8f1fc4c8
Brandon Hill <brandonjon26@gmail.com>
2021-07-26 14:58:56 -0500

    finished setting up my project

---
+++ Take-A-Hike!/Repositories/UserRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Take_A_Hike_.Repositories
+{
+    public class UserRepository
+    {
+    }
+}
