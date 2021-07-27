ff9b64592f302e5f4fa3def22d2b60842238be2a
Brandon Hill <brandonjon26@gmail.com>
2021-07-27 15:59:42 -0500

    almost done with login and register

---
+++ TakeAHike/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TakeAHike.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUsersRepository _usersRepository;
+        public UsersController(IUsersRepository usersRepository)
+        {
+            _usersRepository = usersRepository;
+        }
+
+        [HttpGet("{firebaseUserId}")]
+        public IActionResult GetUser(string firebaseUserId)
+        {
+            return Ok(_usersRepository.GetByFirebaseUserId(firebaseUserId));
+        }
+
+        [HttpGet("DoesUserExist/{firebaseUserId}")]
+        public IActionResult DoesUserExist(string firebaseUserId)
+        {
+            var userProfile = _usersRepository.GetByFirebaseUserId(firebaseUserId);
+            if (userProfile == null)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post(Users users)
+        {
+            users.UserTypeId = UserType.AUTHOR_ID;
+            _usersRepository.Add(users);
+            return CreatedAtAction(
+                nameof(GetUser),
+                new { firebaseUserId = users.FirebaseUserId },
+                users);
+        }
+    }
+}
---
+++ TakeAHike/Repositories/UsersRepository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tabloid.Utils;
+using TakeAHike.Models;
+
+namespace TakeAHike.Repositories
+{
+    public class UsersRepository : BaseRepository, IUsersRepository
+    {
+        public UsersRepository(IConfiguration configuration) : base(configuration) { }
+
+        public Users GetByFirebaseUserId(string firebaseUserId)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT u.Id, u.FireBaseUserId, u.firstName, u.lastName, 
+                               u.email, u.userTypeId,
+                               ut.name AS UserTypeName
+                          FROM users u
+                               LEFT JOIN userType ut on u.userTypeId = ut.Id
+                         WHERE FireBaseUserId = @FireBaseUserId";
+
+                    DbUtils.AddParameter(cmd, "@FirebaseUserId", firebaseUserId);
+
+                    Users users = null;
+
+                    var reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        users = new Users()
+                        {
+                            Id = DbUtils.GetInt(reader, "Id"),
+                            FireBaseUserId = DbUtils.GetString(reader, "FireBaseUserId"),
+                            firstName = DbUtils.GetString(reader, "firstName"),
+                            lastName = DbUtils.GetString(reader, "lastName"),
+                            email = DbUtils.GetString(reader, "email"),
+                            userTypeId = DbUtils.GetInt(reader, "suerTypeId"),
+                            userType = new userType()
+                            {
+                                Id = DbUtils.GetInt(reader, "UserTypeId"),
+                                Name = DbUtils.GetString(reader, "UserTypeName"),
+                            }
+                        };
+                    }
+                    reader.Close();
+
+                    return userProfile;
+                }
+            }
+        }
+    }
+}
