ca34ea3f780a65787d523f0c257210bdbce33b88
Brandon Hill <brandonjon26@gmail.com>
2021-07-27 11:41:42 -0500

    back end for listing parks is done

---
+++ TakeAHike/Controllers/ParkController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TakeAHike.Models;
+using TakeAHike.Repositories;
+
+namespace TakeAHike.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParkController : ControllerBase
+    {
+        private readonly IParkRepository _parksRepository;
+
+        public ParkController(IParkRepository parkRepository)
+        {
+            _parksRepository = parkRepository;
+        }
+
+        // GET: api/<ParksController>
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_parksRepository.GetAllParks());
+        }
+    }
+}
