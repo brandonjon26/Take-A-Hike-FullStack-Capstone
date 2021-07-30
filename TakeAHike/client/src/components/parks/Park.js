8b97f768a932069a9c30fbe9bd855321f09d48b3
Brandon Hill <brandonjon26@gmail.com>
2021-07-30 13:48:10 -0500

    park card updated with delete function and button

--- TakeAHike/client/src/components/parks/Park.js
+++ TakeAHike/client/src/components/parks/Park.js
@@ -4,24 +4,31 @@ import { Button, Card, CardBody } from "reactstrap";
 import { getAllTags } from "../../modules/parkManager";
 import { Link } from "react-router-dom";
 
-export const Parks = ({ park }) => {
+export const Parks = ({ park, handleDeletePark }) => {
     const history = useHistory();
 
-    return (
-        <Card >
-            <CardBody >
-                <div>
-                    <p>
-                        <strong>{park.parkName}</strong>
-                    </p>
-                    <p>{park.contactInfo}</p>
-                </div>
-                <Link to={`/Park/edit/${park.id}`}>
-                    <button className="btn btn-primary">
-                        Edit
-                    </button>
-                </Link>
-            </CardBody>
-        </Card >
-    )
+    if (park.isDeleted === false) {
+        return (
+            <Card >
+                <CardBody >
+                    <div>
+                        <p>
+                            <strong>{park.parkName}</strong>
+                        </p>
+                        <p>{park.contactInfo}</p>
+                    </div>
+                    <Link to={`/Park/edit/${park.id}`}>
+                        <button className="btn btn-primary">
+                            Edit
+                        </button>
+                        <button className="buttonRemovePark" type="button" onClick={() => handleDeletePark(park.id)}>
+                            Delete Park
+                        </button>
+                    </Link>
+                </CardBody>
+            </Card >
+        );
+    } else {
+        return <> </>
+    }
 }
