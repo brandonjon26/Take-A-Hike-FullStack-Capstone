323a784ea6ef8dab24eb2922ab151be35d23e2c2
Brandon Hill <brandonjon26@gmail.com>
2021-08-02 10:40:35 -0500

    hike model created

---
+++ TakeAHike/Models/Hike.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TakeAHike.Models
+{
+    public class Hike
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int ParkId { get; set; }
+        public DateTime DateOdHike { get; set; }
+    }
+}
