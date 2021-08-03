e7b65ff0146f77d92d2a89dd5731e587ea2b66eb
Brandon Hill <brandonjon26@gmail.com>
2021-08-03 13:27:21 -0500

    hike manager functions complete

--- TakeAHike/client/src/modules/hikeManager.js
+++ TakeAHike/client/src/modules/hikeManager.js
@@ -27,3 +27,33 @@ export const addHike = (hike) => {
     });
 };
 
+export const getHikeById = (id) => {
+    return getToken().then((token) => {
+        return fetch(`${baseUrl}/${id}`, {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${token}`,
+            }
+        }).then(resp => {
+            if (resp.ok) {
+                return resp.json();
+            } else {
+                throw new Error("An unknown error occured while getting this hike.")
+            }
+        });
+    });
+}
+
+export const editPark = (hike) => {
+    return getToken().then((token) => {
+        return fetch(`${baseUrl}/${hike.id}`, {
+            method: "PUT",
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "Application/json"
+            },
+            body: JSON.stringify(hike)
+        })
+    });
+}
+
