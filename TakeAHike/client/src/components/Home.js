f43647499edb1231516ba31be10343ef700fb74e
Brandon Hill <brandonjon26@gmail.com>
2021-08-04 09:35:17 -0500

    home page updated, not done yet

--- TakeAHike/client/src/components/Home.js
+++ TakeAHike/client/src/components/Home.js
@@ -1,14 +1,28 @@
 import React from "react";
+import { HikeList } from "./hikes/HikeList";
 
 export default function Hello() {
     return (
-        <span style={{
-            position: "fixed",
-            left: 0,
-            right: 0,
-            top: "50%",
-            marginTop: "-0.5rem",
-            textAlign: "center",
-        }}>hello</span>
+        <div className="header">
+            <h1>Take A Hike!</h1>
+
+            <p>
+                At "Take A Hike!", we aim to give users a great experience and easy way to view parks/trails in their
+                area, while also allowing them to plan their next hiking trip. Our mission is simple, we want to give
+                people with the same love of hiking an easy way to share their passion. What if there is a park not on
+                the website? Simple, just go add it via our "Our Parks" tab! We hope that you find our website simple
+                and straight to the point. Now, quite reading and Take A Hike!
+            </p>
+
+            <HikeList />
+        </div>
+        // <span style={{
+        //     position: "fixed",
+        //     left: 0,
+        //     right: 0,
+        //     top: "50%",
+        //     marginTop: "-0.5rem",
+        //     textAlign: "center",
+        // }}>hello</span>
     );
 }
