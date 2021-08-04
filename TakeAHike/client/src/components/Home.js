5c313a131f2f8532798fa6f18d5cdfa08a026d5d
Brandon Hill <brandonjon26@gmail.com>
2021-08-04 15:08:23 -0500

    updated mission statement

--- TakeAHike/client/src/components/Home.js
+++ TakeAHike/client/src/components/Home.js
@@ -12,9 +12,9 @@ export default function Hello() {
             <p>
                 At "Take A Hike!", we aim to give users a great experience and easy way to view parks/trails in their
                 area, while also allowing them to plan their next hiking trip. Our mission is simple, we want to give
-                people with the same love of hiking an easy way to share their passion. What if there is a park not on
-                the website? Simple, just go add it via our "Our Parks" tab! We hope that you find our website simple
-                and straight to the point. Now, quite reading and Take A Hike!
+                people with the same love of hiking an easy way to share their passion. What if you know or have a
+                park that is not on the website? Simple, just go add it via our "Our Parks" tab! We hope that you
+                find our website simple and straight to the point. Now, quit reading and Take A Hike!
             </p>
             <HikeList />
         </div>
